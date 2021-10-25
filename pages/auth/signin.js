9699fb07b85f731e1e954086fcfab95cddfa8450
MOHAMED HAJJI <pytaek1@gmail.com>
2021-10-25 17:10:46 +0100

    insta comm

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -6,27 +6,33 @@ function signin({ providers }) {
         <>
             <Header />
             <div className="flex flex-col items-center justify-center min-h-screen py-2 -mt-56 px-14 text-center">
-                <img
-                    className="w-80"
-                    src="https://links.papareact.com/ocw"
-                    alt=""
-                />
-                <p className="font-xs italic">Intagram login</p>
-                <div className="mt-40">
-                    {Object.values(providers).map((provider) => (
-                        <div key={provider.name}>
-                            <button
-                                className="p-3 bg-blue-500 rounded-lg text-white"
-                                onClick={() =>
-                                    SignInProvider(provider.id, {
-                                        callbackUrl: '/',
-                                    })
-                                }
-                            >
-                                Sign in with {provider.name}
-                            </button>
+                <div className="mt-40 pt-40 relative flex flex-col items-center space-x-5 md:flex-row">
+                    <div className="left__login">
+                        <img src="../login.png" alt="" />
+                    </div>
+                    <div className="right__login">
+                        <img
+                            className="pt-7"
+                            src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+                            alt=""
+                        />
+                        <div className="mt-7 flex flex-col space-y-5">
+                            {Object.values(providers).map((provider) => (
+                                <div key={provider.name}>
+                                    <button
+                                        className="p-3 bg-[#f99d4c] rounded-lg text-white"
+                                        onClick={() =>
+                                            SignInProvider(provider.id, {
+                                                callbackUrl: '/',
+                                            })
+                                        }
+                                    >
+                                        Sign in with {provider.name}
+                                    </button>
+                                </div>
+                            ))}
                         </div>
-                    ))}
+                    </div>
                 </div>
             </div>
         </>
@@ -42,3 +48,4 @@ export async function getServerSideProps() {
 }
 
 export default signin;
+/*    */
